bcc62dcafecb7146be2213744d7d9cb2c8ab17c4
Erik van der Boom <evdboom@hotmail.com>
2022-11-30 23:32:36 +0100

    Add support for custom

---
+++ Option-A.Blog.Components/Custom/CustomBuilder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using OptionA.Blog.Components.Core;
+
+namespace OptionA.Blog.Components.Custom
+{
+    /// <summary>
+    /// Builder
+    /// </summary>
+    /// <typeparam name="Parent"></typeparam>
+    public class CustomBuilder<Parent> : ContentBuilderBase<CustomBuilder<Parent>, Parent, CustomContent>
+        where Parent : IParentBuilder
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="parent"></param>
+        public CustomBuilder(Parent parent) : base(parent)
+        {
+        }
+
+        /// <summary>
+        /// Sets the fragment to render
+        /// </summary>
+        /// <param name="fragment"></param>
+        /// <returns></returns>
+        public CustomBuilder<Parent> WithFragment(RenderFragment fragment)
+        {
+            _content.Fragment = fragment;
+            return this;
+        }
+
+        /// <inheritdoc/>
+        protected override CustomBuilder<Parent> This()
+        {
+            return this;
+        }
+    }
+}
---
+++ Option-A.Blog.Components/Custom/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using OptionA.Blog.Components.Core;
+
+namespace OptionA.Blog.Components.Custom
+{
+    /// <summary>
+    /// Extensions for the custom classes
+    /// </summary>
+    public static class Extensions
+    {
+        /// <summary>
+        /// Creates a new custom builder
+        /// </summary>
+        /// <typeparam name="Parent"></typeparam>
+        /// <param name="parent"></param>
+        /// <returns></returns>
+        public static CustomBuilder<Parent> CreateCustom<Parent>(this Parent parent) where Parent : IParentBuilder
+        {
+            return new CustomBuilder<Parent>(parent);
+        }
+
+        /// <summary>
+        /// Adds the component to the parent, sets the parameters
+        /// </summary>
+        /// <typeparam name="Parent"></typeparam>
+        /// <param name="parent"></param>
+        /// <param name="componentType"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static Parent AddCustom<Parent>(this Parent parent, Type componentType, params KeyValuePair<string, object?>[] parameters) 
+            where Parent : IParentBuilder
+        {
+            return CreateCustom(parent)
+                .WithFragment(CreateFragment(componentType, parameters))
+                .Build();            
+        }
+
+        private static RenderFragment CreateFragment(Type componentType, params KeyValuePair<string, object?>[] parameters) => builder =>
+        {
+            builder.OpenComponent(0, componentType);
+            int attrCount = 1;
+            foreach (var parameter in parameters)
+            {
+                builder.AddAttribute(attrCount++, parameter.Key, parameter.Value);
+            }
+            builder.CloseComponent();
+        };
+    }
+}
